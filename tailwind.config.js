418a25aa8b187ba7212719268c2316213b7a3a4b
arifatandika25 <arifatandika1234567@gmail.com>
2025-01-08 01:46:39 +0700

    portofolio

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      fontFamily: {
+        Inter: ['Inter'],
+        Lobster: ['Lobster', 'sans-serif'],
+        Jost: ['Jost', 'serif'],
+        Lobster2: ['Lobster Two', 'serif'],
+        poppins: ['poppin', 'serif'],
+        nunto : ['nunto', 'sans-serif'],
+        
+      },
+      colors: {
+        primary:'#1d4ed8',
+        dark : '#0f172a',
+        sec: '#f8fafc',
+        tex : '#64748b'
+      },
+      screens: {
+        '2xl' : '1320px',
+      }
+    },
+  },
+  plugins: [
+  ],
+}
+
