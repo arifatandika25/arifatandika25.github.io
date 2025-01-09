eb2d2bb7826b080cd28fa25074c5975234881038
arifatandika25 <arifatandika1234567@gmail.com>
2025-01-10 05:45:04 +0700

    update unit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html'],
+  content: ['./index.html', './node_modules//flowbite/**/*.js'],
+
   theme: {
     container: {
       center: true,
